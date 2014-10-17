9a5e553a0af7e6534477f7d7de04b6e33fa0b3dc
Jared Hoberock <jaredhoberock@gmail.com>
2014-10-17 16:22:12 -0700

    Enable demo.cpp to compile with clang

--- demo.cpp
+++ demo.cpp
@@ -1,48 +1,40 @@
 #include <coordinate>
-#include <iostream>
 #include <typeinfo>
 
 int main()
 {
-  std::int3 no_args;
+  std::int1 fill_with_ones1(1);
+  std::cout << "fill_with_ones1: " << fill_with_ones1 << std::endl;
 
-  std::cout << "no_args: " << no_args[0] << ", " << no_args[1] << ", " << no_args[2] << std::endl;
+  std::int3 fill_with_ones3(1);
+  std::cout << "fill_with_ones3: " << fill_with_ones3 << std::endl;
+
+  std::int3 no_args;
+  std::cout << "no_args: " << no_args << std::endl;
 
   std::int3 x(1,2,3);
-
   std::int3 copy_of_x = x;
+  std::cout << "copy_of_x: " << copy_of_x << std::endl;
 
-  std::cout << "copy_of_x: " << copy_of_x[0] << ", " << copy_of_x[1] << ", " << copy_of_x[2] << std::endl;
-  
   std::float3 y(4.1,5.1,6.1);
-
-  std::cout << "x[1]: " << x[1] << std::endl;
-
-  std::cout << "y[2]: " << y[2] << std::endl;
+  std::cout << "y: " << y << std::endl;
 
   auto sum = x + y;
-
   std::cout << "type of sum: " << typeid(sum).name() << std::endl;
-
-  std::cout << "sum: " << sum[0] << ", " << sum[1] << ", " << sum[2] << std::endl;
+  std::cout << "sum: " << sum << std::endl;
 
   auto product = x * y;
-
   std::cout << "type of product: " << typeid(product).name() << std::endl;
-
-  std::cout << "product: " << product[0] << ", " << product[1] << ", " << product[2] << std::endl;
+  std::cout << "product: " << product << std::endl;
 
   std::float3 one(1);
-
-  std::cout << "one[2]: " << one[2] << std::endl;
+  std::cout << "one: " << one << std::endl;
 
   auto multiply_by_scalar = 5 * x;
-
   std::cout << "multiply_by_scalar: " << multiply_by_scalar << std::endl;
 
   std::float4 four_float_ones{1,1,1,1};
   std::int3 three_int_zeros{0,0,0};
-
   std::cout << "zeros are less than ones: " << (three_int_zeros < four_float_ones) << std::endl;
 
   return 0;
